3dbfcee040f6152662b6f2dda1e8d3e0f9a37537
Adam Benabou <adam.benabou@student.isae-supaero.fr>
2024-04-14 00:52:48 +0200

    Definition des interfaces admins et simplification du modèle pour s'occuper dans un premier temps des gares et trajets

--- reservations/booking/admin.py
+++ reservations/booking/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Gare, Trajet
 
+
+
+class TrajetAdmin(admin.ModelAdmin):
+    list_display = ('__str__','date_depart', 'date_arrivee')
+    list_filter = ('gare_depart', 'gare_arrivee')
+    search_fields = ('gare_depart', 'gare_arrivee')
+    fieldsets = (
+        (None, { 'fields': ['gare_depart', 'gare_arrivee']}),
+        ("Date information", {'fields': ['date_depart','date_arrivee']}),
+        
+    )
+    
+class GareAdmin(admin.ModelAdmin):
+    list_filter = ('ville',)
+    search_fields = ('nom', 'ville')
+    list_display = ('nom', 'ville')
 # Register your models here.
+admin.site.register(Gare, GareAdmin)
+admin.site.register(Trajet, TrajetAdmin)
--- reservations/booking/models.py
+++ reservations/booking/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
+
+'''
 class Passager(models.Model):
     nom = models.CharField(max_length=100)
     prenom = models.CharField(max_length=100)
@@ -13,12 +15,27 @@ class Client(models.Model) :
     email = models.EmailField()
     telephone = PhoneNumberField( max_length=15)
     
+
+'''
+
+class Gare(models.Model):
+    nom = models.CharField(max_length=255)
+    ville = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.nom
+    
 class Trajet(models.Model):
-    gare_depart = models.CharField(max_length=255)
-    gare_arrivee = models.CharField(max_length=255)
+    gare_depart = models.ForeignKey(Gare, on_delete=models.CASCADE, related_name='gare_depart')
+    gare_arrivee = models.ForeignKey(Gare, on_delete=models.CASCADE, related_name='gare_arrivee')
     date_depart = models.DateTimeField()
     date_arrivee = models.DateTimeField()
     
+    def __str__(self):
+        return self.gare_depart.nom + ' - ' + self.gare_arrivee.nom
+    
+    
+'''
 class Reservation(models.Model):
     date_reservation = models.DateTimeField()
     numero_reservation = models.IntegerField()
@@ -27,6 +44,8 @@ class Reservation(models.Model):
     passager = models.ForeignKey(Passager, on_delete=models.CASCADE)
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     trajet = models.ForeignKey(Trajet, on_delete=models.CASCADE)
+'''
+
     
     
 # Create your models here.

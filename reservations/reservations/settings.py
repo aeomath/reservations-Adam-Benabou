07654e840459be6ac86db12c606b09ca795a86ee
Adam Benabou <adam.benabou@student.isae-supaero.fr>
2024-04-21 00:41:26 +0200

    Mise en forme du menu (marges,espacement,couleur)

--- reservations/reservations/settings.py
+++ reservations/reservations/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'booking/static']
+STATICFILES_DIRS = [BASE_DIR / 'booking' / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

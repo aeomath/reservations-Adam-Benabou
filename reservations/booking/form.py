0bbc6146b52c433353fd30661316f84c7bb98313
Adam Benabou <adam.benabou@student.isae-supaero.fr>
2024-04-25 14:04:52 +0200

    Correction d'un bug

--- reservations/booking/form.py
+++ reservations/booking/form.py
@@ -12,8 +12,8 @@ class SearchForm(forms.Form):
 
 class ReservationForm(forms.ModelForm):
     ## on surcharge la méthode __init__ pour pouvoir passer un client en paramètre et pouvoir filtrer les passagers
-    def __init__(self, client = None , **kwargs):
-        super(ReservationForm, self).__init__(**kwargs)
+    def __init__(self, client = None , *args, **kwargs):
+        super(ReservationForm, self).__init__(*args , **kwargs)
         if client:
             self.fields['passager'].queryset = Passager.objects.filter(client=client)
     class Meta:

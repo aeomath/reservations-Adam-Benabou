fe5321e8f61fcbb9a6f60bfcf0eda7a9cfb3037a
LOIEZ Pierre <p.loiez@pcb61-08-08.isae.fr>
2024-04-29 14:49:09 +0200

    Ajout de max_passagers sur modele Trajets

--- reservations/booking/models.py
+++ reservations/booking/models.py
@@ -35,10 +35,12 @@ class Gare(models.Model):
 class Trajet(models.Model):
     gare_depart = models.ForeignKey(Gare, on_delete=models.CASCADE, related_name='gare_depart')
     gare_arrivee = models.ForeignKey(Gare, on_delete=models.CASCADE, related_name='gare_arrivee')
+    
     date_depart = models.DateTimeField()
     date_arrivee = models.DateTimeField()
-    
+    max_passagers = models.IntegerField()
     distance = models.IntegerField()
+    
     ## ne fonctionne pas pour l'instant
     if date_arrivee < date_depart:
         raise ValidationError("La date d'arrivée doit être supérieure à la date de départ")
@@ -77,7 +79,7 @@ class Reservation(models.Model):
     date_reservation = models.DateTimeField()
     numero_reservation = models.AutoField(primary_key=True)
     numero_voiture = models.IntegerField()
-    numero_place = models.IntegerField()
+    numero_place = models.IntegerField() ##Ne doit pas dépasser le nombre max de passagers
     passager = models.ForeignKey(Passager, on_delete=models.CASCADE)
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     trajet = models.ForeignKey(Trajet, on_delete=models.CASCADE)
@@ -85,7 +87,7 @@ class Reservation(models.Model):
     ## rajout de la contrainte d'unicité pour la place par trajet (code trouvé sur le net)
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['trajet', 'numero_place'], name='unique_place_par_trajet')
+            models.UniqueConstraint(fields=['trajet', 'numero_place'], name='unique_place_par_trajet'),
         ]
     def __str__(self):
         return str(self.trajet)
